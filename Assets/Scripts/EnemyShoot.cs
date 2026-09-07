a2a8f4d6339bdd2a78bcc0b334a874182aaf457e
masonm224 <masonm224@icloud.com>
2026-09-07 01:00:32 -0400

    EnemyAimming down
    I have a ray marking where the enemy is aiming. Disable gizmos to get rid of it.
    Enemy Aim added
    enemy shoot coroutine added
    
    gonna do basic UI and level/room loading before I start overhauling player and enemy stuff

--- Assets/Scripts/EnemyShoot.cs
+++ Assets/Scripts/EnemyShoot.cs
@@ -1,37 +1,69 @@
 using UnityEngine;
 
+//Important for running the Co
+using System.Collections;
+
 public class EnemyShoot : MonoBehaviour
 {
 
+        //This allows you to assign a FirePoint in the inspector (invisible object for direction of bullet travel)
+    public Transform FirePoint;
 
-
+    //Prefab Variant of player bullet
     [SerializeField] private GameObject EnemyBullet;
-    [SerializeField] private GameObject TargetForEnemy;
+
+    //This is an empty that is a child to the player
+    private Transform TargetForEnemy;
     
-
-
     void Start()
     {
-        
+        //This makes every new enemy with this script know players location without needing to assign in inspector everytime
+        TargetForEnemy = GameObject.FindGameObjectWithTag("Player").transform;
+
+        //this will move to be activated and deactivated by room starting later
+        StartCoroutine(EnemyShootCoroutine());
     }
 
-    
+    IEnumerator EnemyShootCoroutine()
+    {
+        while (true)
+        {
+            EnemyShooting();
 
-
-
-
+            yield return new WaitForSeconds(2f);
+        }
+    }
 
     void Update()
     {
-        
+        EnemyAim();
     }
 
-    public void Shooting()
+    public void EnemyAim()
+    {
+        if (TargetForEnemy != null)
+        {
+            //rotates the enemy to aim at the player 
+            Vector3 direction = TargetForEnemy.position - FirePoint.position;
+
+            direction.y = 0;
+
+            transform.forward = direction;
+
+            //Debug log line to show where the bullet is aiming 
+            Debug.DrawRay(FirePoint.position, FirePoint.forward * 15f, Color.red);
+        }
+        
+        else
+        {
+        Debug.Log("TargetForEnemyNotFound");
+        }
+    }
+
+    public void EnemyShooting()
     {
 
-        Instantiate(bulletPrefab, FirePoint.position, FirePoint.rotation);
+        Instantiate(EnemyBullet, FirePoint.position, FirePoint.rotation);
 
     }   
-
-
 }
